98b0cedb2083eccd2c2c6816759d6ed88bbdf111
Elena <esowesp@epsg.upv.es>
2023-04-28 21:15:35 +0200

    js tabla comercial

---
+++ src/app/js/page_comercial.js
@@ -0,0 +1,101 @@
+function mostrarInput(element){
+    element.parentNode.classList.add("activo");
+}
+
+function ocultarInput(element) {
+    element.parentNode.classList.remove("activo");
+}
+
+let solicitudesPorPagina = 15;
+
+let pagina = 1;
+
+let solicitudes = [];
+
+const contenedor = document.getElementById("tabla-solicitudes-pendientes");
+
+//------------------------------------------------------------
+/*
+Función obtenerDatos()
+*/
+//------------------------------------------------------------
+/*
+async function obtenerDatos() {
+    const respuesta = await fetch("../../api/cien_datos_para_tabla.json");
+    const data = await respuesta.json();
+    solicitudes = data;
+}
+*/
+
+(async () => {
+    const respuesta = await fetch("../../../api/cien_datos_para_tabla.json");
+    const data = await respuesta.json();
+    solicitudes = data;
+
+    let ultimaPagina = Math.ceil(solicitudes.length / solicitudesPorPagina);
+    let ultimoBoton = document.querySelector("#paginador > button:last-child")
+    ultimoBoton.innerText = ultimaPagina;
+    ultimoBoton.onclick = () => cambiarPagina(ultimaPagina);
+
+    let inputPagina = document.querySelector("#paginador input");
+    inputPagina.max = ultimaPagina;
+    inputPagina.min = 1;
+
+    cambiarPagina(1);
+})();
+
+//------------------------------------------------------------
+/*
+Función crearFilaSolicitud()
+*/
+//------------------------------------------------------------
+function crearFilaSolicitud(solicitud) {
+    let fila = document.createElement("tr");
+
+    let celdaId = document.createElement("td");
+    celdaId.textContent = solicitud.datos.id;
+
+    let celdaEmail = document.createElement("td");
+    celdaEmail.textContent = solicitud.datos.email;
+
+    let celdaNombre = document.createElement("td");
+    celdaNombre.textContent = solicitud.datos.nombre;
+
+    fila.append(celdaId, celdaEmail, celdaNombre);
+
+    return fila;
+}
+
+//------------------------------------------------------------
+/*
+Función main():
+*/
+//------------------------------------------------------------
+/*
+async function main(){
+    await obtenerDatos();
+
+ */
+/*
+    for(let i = 0; i<solicitudes.length; i++){
+        let solicitud = solicitudes[i];
+        if(solicitud){
+            contenedor.appendChild(crearFilaSolicitud(solicitud));
+        }
+    }
+}
+
+ */
+
+function cambiarPagina(numeroPagina) {
+    pagina = numeroPagina;
+    contenedor.innerHTML = "";
+    let inicio = (pagina - 1) * solicitudesPorPagina;
+    for (let i = inicio; i < inicio + solicitudesPorPagina; i++) {
+        let solicitud = solicitudes[i];
+        if(solicitud) contenedor.appendChild(crearFilaSolicitud(solicitud)); //conversión implícita de tipos (ventas no es un booleano, pero aquí, si venta
+        //contiene un objeto, considera que es verdadero. Si es undefined (variable que no tiene valor asignado), lo considera falso.
+    }
+    let inputPagina = document.querySelector("#paginador input");
+    inputPagina.value = pagina;
+}
