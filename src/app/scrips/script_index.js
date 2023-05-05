2c83e4dfb6309604c76d4e5488854acd214cd2ae
Pmg <pmeagon@epsg.upv.es>
2023-05-05 03:36:21 +0200

    intento iniciar sesion fallido

--- src/app/scrips/script_index.js
+++ src/app/scrips/script_index.js
@@ -1,8 +1,22 @@
-const btnRegresarInicio = document.getElementById('btn-regresar-inicio');
+function validar() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-btnRegresarInicio.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
+    if (usuario === "" || contrasena === "") {
+        alert("Por favor, ingrese el usuario y la contraseña");
+        return false;
+    }
+    // Validación de usuarios y contraseñas
+    if (username === "usuario" && password === "1234567890") {
+        window.location = "../html/page_usuario/page_usuario.html"; // Redirige al usuario a la pagina de usuario
+    } else if (username === "admin" && password === "1234567890") {
+        window.location.href = "../html/page_admin/page_admin.html"; // Redirige al administrador a la pagina de administrador
+    }else if (username === "tecnico" && password === "1234567890") {
+        window.location.href = "../html/page_tecnico/page_tecnico.html"; // Redirige al tecnico a la página de tecnico
+    }else if (username === "comercial" && password === "1234567890") {
+        window.location.href = "../html/page_comercial/page_comercial.html"; // Redirige al comercial a la página  de comercial
+    } else {
+        alert("Usuario o contraseña incorrectos");
+    }
+
+}
