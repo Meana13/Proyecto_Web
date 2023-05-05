7b211f44c42a50a3aa88540c63dcc040bd013f26
Pmg <pmeagon@epsg.upv.es>
2023-05-05 10:15:36 +0200

    Iniciar sesion completo vamoooo

--- src/app/scrips/script_index.js
+++ src/app/scrips/script_index.js
@@ -4,14 +4,20 @@ function validarFormulario(event) {
     const contrasenya = document.getElementById("contrasenya").value;
     const usuariosGuardados = {
         // aquí se añadirían los usuarios y contraseñas guardados en la carpeta "usuarios"
-        admin: "1234",
-        tecnico: "5678",
+        admin: "1234567890",
+        tecnico: "1234567890",
         usuario: "1234567890",
+        comercial:"1234567890",
     };
-    if (usuariosGuardados[usuario] === contrasenya) {
+    if (usuario === "usuario" && usuariosGuardados[usuario] === contrasenya) {
         window.location.href = "../../estilos_page_tecnico.css/src/app/html/page_usuario/page_usuario.html"; // redirecciona a la página de carga si el usuario y contraseña son correctos
-    }
-    else {
+    } else if (usuario === "admin" && contrasenya === usuariosGuardados[usuario]) {
+        window.location.href = "../../estilos_page_tecnico.css/src/app/html/page_admin/page_admin.html";
+    } else if (usuario === "tecnico" && contrasenya === usuariosGuardados[usuario]) {
+        window.location.href = "../../estilos_page_tecnico.css/src/app/html/page_tecnico/page_tecnico.html";
+    } else if (usuario == "comercial" && contrasenya === usuariosGuardados[usuario]) {
+        window.location.href = "../../estilos_page_tecnico.css/src/app/html/page_comercial/page_comercial.html";
+    } else {
         const popup = document.getElementById("popup");
         popup.style.display = "block"; // muestra el popup si el usuario y contraseña son incorrectos
         const cerrarPopup = document.getElementById("cerrar-popup");
