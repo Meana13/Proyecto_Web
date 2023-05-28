79edd9e19ea424326575cb85e4f9e968b1264732
Elena <esowesp@epsg.upv.es>
2023-05-28 14:25:04 +0200

    html y css basico usuario hecho

--- src/app/scrips/page_usuario/page_usuario.js
+++ src/app/scrips/page_usuario/page_usuario.js
@@ -58,6 +58,119 @@ botonGuardarAjustesHuerto.addEventListener("click", cerrarAjustesHuerto);
 botonCerrarAjustesHuerto.addEventListener("click", cerrarAjustesHuerto);
 
 
+/*
+    =========================================================================
+              DIÁLOGO ATAJO PARA CAMBIAR LOS LÍMITES DE LAS MEDIDAS
+    =========================================================================
+                                                                                */
+
+//Atajo cambiar límites de SALINIDAD:
+const abrirAtajoSal = document.getElementById('atajo_limites_sal');
+const dialogoAtajoSal = document.getElementById('dialogo_atajo_sal');
+const guardarAtajoSal = document.getElementById('atajo_guardar_limites_sal');
+const cancelarAtajoSal = document.getElementById('atajo_cancelar_limites_sal');
+
+abrirAtajoSal.addEventListener("click", ()=>{
+    dialogoAtajoSal.classList.add('activo');
+    dialogoAtajoSal.showModal();
+});
+
+function cerrarDialogoSal(){
+    dialogoAtajoSal.classList.remove('activo');
+    dialogoAtajoSal.close();
+}
+
+guardarAtajoSal.addEventListener("click", cerrarDialogoSal);
+cancelarAtajoSal.addEventListener("click", cerrarDialogoSal);
+
+//------------------------------------------------------------------------
+
+//Atajo cambiar límites de HUMEDAD:
+const abrirAtajoHumedad = document.getElementById('atajo_limites_humedad');
+const dialogoAtajoHumedad = document.getElementById('dialogo_atajo_humedad');
+const guardarAtajoHumedad = document.getElementById('atajo_guardar_limites_humedad');
+const cancelarAtajoHumedad = document.getElementById('atajo_cancelar_limites_humedad');
+
+abrirAtajoHumedad.addEventListener("click", ()=>{
+    dialogoAtajoHumedad.classList.add('activo');
+    dialogoAtajoHumedad.showModal();
+});
+
+function cerrarDialogoHumedad(){
+    dialogoAtajoHumedad.classList.remove('activo');
+    dialogoAtajoHumedad.close();
+}
+
+guardarAtajoHumedad.addEventListener("click", cerrarDialogoHumedad);
+cancelarAtajoHumedad.addEventListener("click", cerrarDialogoHumedad);
+
+//------------------------------------------------------------------------
+
+//Atajo cambiar límites de PH:
+const abrirAtajoPh = document.getElementById('atajo_limites_ph');
+const dialogoAtajoPh = document.getElementById('dialogo_atajo_ph');
+const guardarAtajoPh = document.getElementById('atajo_guardar_limites_ph');
+const cancelarAtajoPh = document.getElementById('atajo_cancelar_limites_ph');
+
+abrirAtajoPh.addEventListener("click", ()=>{
+    dialogoAtajoPh.classList.add('activo');
+    dialogoAtajoPh.showModal();
+});
+
+function cerrarDialogoPh(){
+    dialogoAtajoPh.classList.remove('activo');
+    dialogoAtajoPh.close();
+}
+
+guardarAtajoPh.addEventListener("click", cerrarDialogoPh);
+cancelarAtajoPh.addEventListener("click", cerrarDialogoPh);
+
+//------------------------------------------------------------------------
+
+//Atajo cambiar límites de TEMPERATURA:
+const abrirAtajoTemperatura = document.getElementById('atajo_limites_temperatura');
+const dialogoAtajoTemperatura = document.getElementById('dialogo_atajo_temperatura');
+const guardarAtajoTemperatura = document.getElementById('atajo_guardar_limites_temperatura');
+const cancelarAtajoTemperatura = document.getElementById('atajo_cancelar_limites_temperatura');
+
+abrirAtajoTemperatura.addEventListener("click", ()=>{
+    dialogoAtajoTemperatura.classList.add('activo');
+    dialogoAtajoTemperatura.showModal();
+});
+
+function cerrarDialogoTemperatura(){
+    dialogoAtajoTemperatura.classList.remove('activo');
+    dialogoAtajoTemperatura.close();
+}
+
+guardarAtajoTemperatura.addEventListener("click", cerrarDialogoTemperatura);
+cancelarAtajoTemperatura.addEventListener("click", cerrarDialogoTemperatura);
+
+//------------------------------------------------------------------------
+
+//Atajo cambiar límites de LUZ:
+const abrirAtajoLuz = document.getElementById('atajo_limites_luz');
+const dialogoAtajoLuz = document.getElementById('dialogo_atajo_luz');
+const guardarAtajoLuz = document.getElementById('atajo_guardar_limites_luz');
+const cancelarAtajoLuz = document.getElementById('atajo_cancelar_limites_luz');
+
+abrirAtajoLuz.addEventListener("click", ()=>{
+    dialogoAtajoLuz.classList.add('activo');
+    dialogoAtajoLuz.showModal();
+});
+
+function cerrarDialogoLuz(){
+    dialogoAtajoLuz.classList.remove('activo');
+    dialogoAtajoLuz.close();
+}
+
+guardarAtajoLuz.addEventListener("click", cerrarDialogoLuz);
+cancelarAtajoLuz.addEventListener("click", cerrarDialogoLuz);
+
+
+
+/*-----------------------------------------------Aquí acaban las funciones para el diálogo de cambiar límites de medidas*/
+
 
 
 
@@ -98,31 +211,6 @@ function cambiarTab(tab) {
 
 /*-----------------------------------------------Aquí acaban las funciones para el visualizador de huertos en pestañas*/
 
-
-/*
-    =========================================================================
-              DIÁLOGO ATAJO PARA CAMBIAR LOS LÍMITES DE LAS MEDIDAS
-    =========================================================================
-                                                                                */
-const botonAbrirDialogo = document.getElementById('boton_dialogo_atajo_limites_medidas');
-const dialogo = document.querySelector('dialog');
-const guardar = document.getElementById('boton_guardar_limites_medidas_dialogo');
-const cancelar = document.getElementById('boton_cancelar_limites_medidas_dialogo');
-
-botonAbrirDialogo.addEventListener("click", ()=>{
-    dialogo.classList.add('activo');
-    dialogo.showModal();
-});
-
-function cerrarDialogo(){
-    dialogo.classList.remove('activo');
-    dialogo.close();
-}
-
-guardar.addEventListener("click", cerrarDialogo);
-cancelar.addEventListener("click", cerrarDialogo);
-
-/*-----------------------------------------------Aquí acaban las funciones para el diálogo de cambiar límites de medidas*/
 
 
 
